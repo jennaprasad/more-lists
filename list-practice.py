2df548dba3df324f2a0a3d8ef03943980a40df13
Jenna Prasad <jprasad@ucvts.tec.nj.us>
2017-11-08 14:57:54 +0000

    Modified exercise 1

--- list-practice.py
+++ list-practice.py
@@ -5,7 +5,7 @@ If you're correct, you'll get a message in the terminal saying "Your string_list
 """
 
 # On the line below, create a list named string_list containing 7 strings
-
+string_list=["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 
 
